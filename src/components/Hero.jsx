71f2cd12de17bfe07ca9c5c5d7882b4ed5ecd6d4
Rohit <rohitdhanotia2003@gmail.com>
2024-08-16 19:19:15 +0530

    Navbar and Hero Section

---
+++ src/components/Hero.jsx
@@ -0,0 +1,52 @@
+import React from "react";
+
+function Hero() {
+  return (
+    <div className="font-sans py-20 dark:bg-primary h-full">
+      <div className="text-center max-w-6xl max-md:max-w-md mx-auto">
+        <div>
+          <h2 className="text-gray-800 dark:text-white md:text-5xl text-4xl font-bold mb-4">
+            Explore the Possibilities of{" "}
+            <span class="text-purple-700">AI Book Writing</span> with BookAI
+          </h2>
+          <div className="md:max-w-xl mx-auto">
+            <p className="text-gray-600 text-center dark:text-gray-300 mt-8 text-lg leading-relaxed">
+              Unleash the power of AI to create captivating books in minutes.
+              Download, Distribute wherever you want. Generate unlimited free
+              Books.
+            </p>
+          </div>
+          <div className="mt-12 flex gap-x-6 gap-y-4 justify-center max-sm:flex-col">
+            <button
+              type="button"
+              className="bg-purple-600 hover:bg-transparent hover:text-purple-600 border border-purple-600 transition-all text-white font-bold text-sm rounded-full px-6 py-3"
+            >
+              Get Started
+            </button>
+            <button
+              type="button"
+              className="bg-purple-600 hover:bg-transparent hover:text-purple-600 border border-purple-600 transition-all text-white font-bold text-sm rounded-full px-6 py-3"
+            >
+              Get API Access
+            </button>
+          </div>
+          <div className="mx-auto mt-4">
+            <p className="dark:text-amber-200 text-amber-500 text-center my-6 text-md leading-relaxed">
+              Attention: TryBookAI has been acquired by Indicus AI. Indicus LLM
+              will be used for text and image generation
+            </p>
+          </div>
+          <div className="max-w-6xl mx-auto bg-purple-900 bg-opacity-20 rounded-lg shadow-lg p-10 my-24 font-[sans-serif] overflow-visible">
+            <img
+              src="https://raw.githubusercontent.com/adarshagupta/trybookai/main/book2.png"
+              className="w-full rounded-lg hover:cursor-pointer hover:scale-105 transition"
+              alt="hero"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Hero;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,143 @@
+import React, { useState, useEffect } from "react";
+
+function Navbar() {
+  const [theme, setTheme] = useState(null);
+
+  useEffect(() => {
+    if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
+      setTheme("dark");
+    }
+  }, []);
+
+  useEffect(() => {
+    if (theme === "light") {
+      document.documentElement.classList.remove("dark");
+    } else {
+      document.documentElement.classList.add("dark");
+    }
+  }, [theme]);
+
+  const toggleTheme = () => {
+    setTheme((prevTheme) => (prevTheme === "light" ? "dark" : "light"));
+  };
+  return (
+    <nav className="bg-gray-50 border-gray-200 dark:bg-primary">
+      <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4 py-8">
+        <a
+          href="https://flowbite.com/"
+          className="flex items-center space-x-3 rtl:space-x-reverse"
+        >
+          <img
+            src="https://flowbite.com/docs/images/logo.svg"
+            className="h-8"
+            alt="Flowbite Logo"
+          />
+          <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
+            BookAI
+          </span>
+        </a>
+        <div className="flex md:order-2 space-x-3 md:space-x-0 gap-8 rtl:space-x-reverse">
+        <label class="inline-flex items-center cursor-pointer">
+          <input
+            type="checkbox"
+            value=""
+            class="sr-only peer"
+            onClick={toggleTheme}
+          />
+          <div class="relative w-14 h-7 bg-gray-200 rounded-full peer dark:bg-purple-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:start-[4px] after:bg-white after:rounded-full after:h-6 after:w-6 after:transition-all dark:border-gray-600 peer-checked:bg-purple-500"></div>
+        </label>
+          <button
+            type="button"
+            className="text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-blue-800"
+          >
+            Get started
+          </button>
+          <button
+            data-collapse-toggle="navbar-cta"
+            type="button"
+            class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+            aria-controls="navbar-cta"
+            aria-expanded="false"
+          >
+            <span className="sr-only">Open main menu</span>
+            <svg
+              className="w-5 h-5"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 17 14"
+            >
+              <path
+                stroke="currentColor"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M1 1h15M1 7h15M1 13h15"
+              />
+            </svg>
+          </button>
+        </div>
+        <div
+          className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1"
+          id="navbar-cta"
+        >
+          <ul className="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-gray-50 dark:bg-primary dark:border-purple-700">
+            <li>
+              <a
+                href="/"
+                className="block py-2 px-3 md:p-0 rounded text-purple-700 dark:hover:text-purple-400 md:dark:text-gray-100"
+                aria-current="page"
+              >
+                Features
+              </a>
+            </li>
+            <li>
+              <a
+                href="/"
+                className="block py-2 px-3 md:p-0 rounded text-purple-700 dark:hover:text-purple-400 md:dark:text-gray-100"
+                aria-current="page"
+              >
+                How it Works
+              </a>
+            </li>
+            <li>
+              <a
+                href="/"
+                className="block py-2 px-3 md:p-0 rounded text-purple-700 dark:hover:text-purple-400 md:dark:text-gray-100"
+                aria-current="page"
+              >
+                Roadmap
+              </a>
+            </li>
+            <li>
+              <a
+                href="/"
+                className="block py-2 px-3 md:p-0 rounded text-purple-700 dark:hover:text-purple-400 md:dark:text-gray-100"
+              >
+                API
+              </a>
+            </li>
+            <li>
+              <a
+                href="/"
+                className="block py-2 px-3 md:p-0 rounded text-purple-700 dark:hover:text-purple-400 md:dark:text-gray-100"
+              >
+                Price
+              </a>
+            </li>
+            <li>
+              <a
+                href="/"
+                className="block py-2 px-3 md:p-0 rounded text-purple-700 dark:hover:text-purple-400 md:dark:text-gray-100"
+              >
+                Models
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
